77c4432f1e8debc8b1d76945b27c3aa639e2966e
Catalystiq <73408095+Catalystiq@users.noreply.github.com>
2022-05-20 12:49:26 -0600

    initial commit with project files
    Initial commit with project files from Visual Studio

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CS_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+
+            //Console.ReadLine();
+        }
+    }
+}
